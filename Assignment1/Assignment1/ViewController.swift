a5064ad73bc7aa1ed0189c60d4f2865ec4578ea5
chintan <agrawal.chintan2@gmail.com>
2023-02-05 13:58:01 -0800

    assignment-1

---
+++ Assignment1/Assignment1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Himanshu Vaghela on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ 
+    @IBOutlet weak var Label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func Clickhere(_ sender: UIButton) {
+        print("Hello World")
+    }
+}
+
